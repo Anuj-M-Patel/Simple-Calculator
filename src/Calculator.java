bfcd00a1078a59851a57c7040c9e2811c01df253
anpatel21 <anpatel21@HS-MATH2-18.gvsd.org>
2018-09-17 13:36:58 -0400

    classwork

---
+++ src/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator 
+{
+	
+	// Public is an access modifier. Who can use this?
+	// Method declaration
+	public int add(int num1, int num2) 
+	{
+	
+		// Parameters are initialized when the method is called.
+		int sum = num1 + num2;
+		return sum;
+	
+	}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Object creation
+		// "new" is a java keyword which creates an object
+		Calculator calculator = new Calculator();
+		// Must include parenthesis because of object declaration
+		// Calculator is the data type
+		// calculator is the variable
+		// new Calculator() is the object
+		
+		System.out.print(calculator.add(2, 6));
+
+	}
+
+}
